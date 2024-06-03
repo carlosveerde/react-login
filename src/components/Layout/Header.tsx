95db81de39548c8e10e2481fc7cbaee984de97e2
Verde <carlos-verde@outlook.com>
2024-06-03 15:55:32 -0300

    style: :lipstick: Update Header component layout and styles

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,20 +1,37 @@
 import Logo from '../../assets/images/Logo.svg';
+import LogoLight from '../../assets/images/Logo-light.svg';
+import { useTheme } from '../../contexts/Theme/ThemeContext';
+import ToogleThemeSwitch from '../UI/ToggleThemeSwitch';
+import { useSlider } from "../../contexts/Slider/SliderContext";
+import OpenSliderButton from '../Auth/OpenSliderButton';
 
 export default function Header() {;
+  const { isDarkMode } = useTheme();
+  const { isOpen } = useSlider();
 
   return (
-    <div className="flex justify-between items-center">
-      <div>
-        <img
-          alt=""
-          className="h-8"
-          src={Logo}
-        />
+    <div className='grid grid-rows-2 grid-flow-col gap-4 -mt-6'>
+      <div className='flex flex-row-reverse justify-between items-center'>
+        <div>
+          <ToogleThemeSwitch />
+        </div>
+        <div>{
+          !isOpen ? <OpenSliderButton /> : null
+        }</div>
       </div>
-      <div>
-        <a href="#" className="text-blue-500 size-5 px-5 py-4">
-          Criar conta
-        </a>
+      <div className="flex justify-between items-center">
+        <div>
+          <img
+            alt=""
+            className="h-8"
+            src={isDarkMode ? LogoLight : Logo}
+          />
+        </div>
+        <div>
+          <a href="#" className="text-blue-500 size-5">
+            Criar conta
+          </a>
+        </div>
       </div>
     </div>
   );
